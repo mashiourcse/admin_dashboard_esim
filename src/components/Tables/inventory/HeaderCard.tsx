9672adeeb096ad4952d5ef5da9922130b391413d
mashintlab <mashiour.cseiiuc@gmail.com>
2025-08-21 20:43:36 +0600

    fix error attempt

--- src/components/Tables/inventory/HeaderCard.tsx
+++ src/components/Tables/inventory/HeaderCard.tsx
@@ -27,7 +27,7 @@ const HeaderCard: React.FC<AddCardProps> = ({
         <div className="bg-white rounded-lg shadow-md p-6 flex flex-row items-start justify-between dark:bg-[#122031]">
             <div>
                 <h2 className='text-[20px] font-bold leading-[30px] text-dark dark:text-white'>Inventory Management</h2>
-            <p>Here's you can access all the information about the registered sims</p>
+            <p>Here you can access all the information about the registered sims</p>
             </div>
           <Button type="primary" 
     style={{ marginBottom: 0, marginTop: 16 }} 
