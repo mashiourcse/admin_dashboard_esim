b5c14cc7068e8f6ac4f6c1f55f7df0db9ef0b1d4
mashintlab <mashiour.cseiiuc@gmail.com>
2025-08-21 20:50:10 +0600

    fix attempt 2

--- src/app/inventory/sim-dashboard/[sim_id]/page.tsx
+++ src/app/inventory/sim-dashboard/[sim_id]/page.tsx
@@ -1,20 +1,21 @@
+import { NextPage } from "next";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import SimDashboard from "@/components/SimCard";
 
-import React from "react";
-
+// You can get `params` directly from the context in Next.js 13
 interface SimDashboardPageProps {
-    params: { sim_id: string };
+  params: { sim_id: string };
 }
 
-const SimDashboardPage: React.FC<SimDashboardPageProps> = ({ params }) => {
-    const { sim_id } = params;
-    return (
-        <div>
-          <Breadcrumb pageName={`SIM Dashboard`} />
-            <SimDashboard />
-        </div>
-    );
+const SimDashboardPage: NextPage<SimDashboardPageProps> = ({ params }) => {
+  const { sim_id } = params;
+
+  return (
+    <div>
+      <Breadcrumb pageName={`SIM Dashboard`} />
+      <SimDashboard />
+    </div>
+  );
 };
 
 export default SimDashboardPage;
