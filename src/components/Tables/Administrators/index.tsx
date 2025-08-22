e41893eb0c0c1012abcb9eb86bc8b322ca6b5a95
Nirjhor3029 <sazzad3029@gmail.com>
2025-08-22 20:05:33 +0600

    admin user list show

--- src/components/Tables/Administrators/index.tsx
+++ src/components/Tables/Administrators/index.tsx
@@ -1,10 +1,26 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { Table, Button, Space, ConfigProvider, Modal, Card, Row, Form, Input, message } from 'antd';
-import { EditOutlined, DeleteOutlined, InfoCircleOutlined, CopyOutlined } from '@ant-design/icons';
-import { useTheme } from 'next-themes';
-import { theme } from 'antd';
-import AddCard from './AddCard';
+import axiosInstance from "@/api/axios";
+import {
+  CopyOutlined,
+  DeleteOutlined,
+  InfoCircleOutlined,
+} from "@ant-design/icons";
+import {
+  Button,
+  Card,
+  ConfigProvider,
+  Form,
+  Input,
+  message,
+  Modal,
+  Row,
+  Space,
+  Table,
+  theme,
+} from "antd";
+import { useTheme } from "next-themes";
+import React, { useEffect, useState } from "react";
+import AddCard from "./AddCard";
 
 // Interface for user data
 interface UserData {
@@ -12,8 +28,8 @@ interface UserData {
   name: string;
   email: string;
   role: string;
-  signupDate?: string; // Optional signup date
-  phoneNumber?: string; // Add optional phone number
+  signupDate: string; // Optional signup date
+  // phoneNumber?: string; // Add optional phone number
 }
 
 interface UserColumn {
@@ -23,7 +39,7 @@ interface UserColumn {
   render?: (value: any, record: UserData, index: number) => React.ReactNode;
 }
 
-interface ActionColumn extends Omit<UserColumn, 'render'> {
+interface ActionColumn extends Omit<UserColumn, "render"> {
   render: (_: any, record: UserData) => React.ReactNode;
 }
 
@@ -34,7 +50,6 @@ const UsersTable: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
 
-
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalData, setModalData] = useState<UserData | null>(null);
 
@@ -42,27 +57,30 @@ const UsersTable: React.FC = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [formData, setFormData] = useState<UserData | null>(null);
 
+  const [data, setData] = useState<UserData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     setMounted(true);
   }, []);
 
   useEffect(() => {
     if (mounted) {
-      if (currentTheme === 'dark') {
+      if (currentTheme === "dark") {
         setAntTheme({
           token: {
-            colorPrimary: '#1D1D1D',
-            colorBgBase: '#020D1A',
-            colorTextBase: '#FFFFFF',
+            colorPrimary: "#1D1D1D",
+            colorBgBase: "#020D1A",
+            colorTextBase: "#FFFFFF",
           },
           algorithm: theme.darkAlgorithm,
         });
       } else {
         setAntTheme({
           token: {
-            colorPrimary: '#1890ff',
-            colorBgBase: '#FFFFFF',
-            colorTextBase: '#000000',
+            colorPrimary: "#1890ff",
+            colorBgBase: "#FFFFFF",
+            colorTextBase: "#000000",
           },
           algorithm: theme.defaultAlgorithm,
         });
@@ -70,40 +88,83 @@ const UsersTable: React.FC = () => {
     }
   }, [currentTheme, mounted]);
 
-  const data: UserData[] = [
-    {
-      key: '1',
-      name: 'Mashior Rahman',
-      email: 'mashior.csei.iuc@gmail.com',
-      role: 'admin',
-      phoneNumber: '0183423432432',
-    },
-    {
-      key: '2',
-      name: 'Nidal Chowdhury',
-      email: 'nidal.chy1993@gmail.com',
-      role: 'admin',
-      phoneNumber: '0183423432432',
-    },
-    {
-      key: '3',
-      name: 'Sibel Gazozcu',
-      email: 'sibel.gazozcu@telna.com',
-      role: 'admin',
-      phoneNumber: '0183423432432',
-    },
-    {
-      key: '4',
-      name: 'Ishaan Dutta',
-      email: 'ishaan@telna.com',
-      role: 'admin',
-      phoneNumber: '0183423432432',
-    },
-  ];
+  // Fetch inventory data from API
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axiosInstance.get("/user?results=10");
+        console.log(response.data.data);
 
-  const handleCopyEmail = (data: { email: string; }) => {
+        const transformedData = response.data.data.map((item: any) => {
+          const formatDate = (dateStr: string) =>
+            new Date(dateStr).toLocaleString("en-GB", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: true,
+            });
+
+          return {
+            _id: item.id,
+            name: item.firstName + " " + item.lastName,
+            email: item.email,
+            // signupDate: formatDate(item.createdAt),
+            role: item.role,
+            createdAt: formatDate(item.createdAt),
+            // phoneNumber: item.phoneNumber,
+          };
+        });
+
+        console.log(transformedData);
+
+        setData(transformedData); // Set the fetched data to state
+      } catch (error) {
+        console.error("Error fetching user data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // const data: UserData[] = [
+  //   {
+  //     key: '1',
+  //     name: 'Mashior Rahman',
+  //     email: 'mashior.csei.iuc@gmail.com',
+  //     role: 'admin',
+  //     phoneNumber: '0183423432432',
+  //   },
+  //   {
+  //     key: '2',
+  //     name: 'Nidal Chowdhury',
+  //     email: 'nidal.chy1993@gmail.com',
+  //     role: 'admin',
+  //     phoneNumber: '0183423432432',
+  //   },
+  //   {
+  //     key: '3',
+  //     name: 'Sibel Gazozcu',
+  //     email: 'sibel.gazozcu@telna.com',
+  //     role: 'admin',
+  //     phoneNumber: '0183423432432',
+  //   },
+  //   {
+  //     key: '4',
+  //     name: 'Ishaan Dutta',
+  //     email: 'ishaan@telna.com',
+  //     role: 'admin',
+  //     phoneNumber: '0183423432432',
+  //   },
+  // ];
+
+  const handleCopyEmail = (data: { email: string }) => {
     navigator.clipboard.writeText(data.email);
-    message.success('Email copied!');
+    message.success("Email copied!");
   };
 
   const handleInfoClick = (record: UserData) => {
@@ -123,7 +184,7 @@ const UsersTable: React.FC = () => {
     console.log("Saving user data:", formData);
     setIsEditing(false);
     setIsModalVisible(false);
-    message.success('User details updated successfully!');
+    message.success("User details updated successfully!");
   };
 
   const handleCancelClick = () => {
@@ -142,47 +203,56 @@ const UsersTable: React.FC = () => {
 
   const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setFormData(prevData => (prevData ? { ...prevData, [name]: value } : null));
+    setFormData((prevData) =>
+      prevData ? { ...prevData, [name]: value } : null,
+    );
   };
 
   const columns: (UserColumn | ActionColumn)[] = [
     {
-      title: '#',
-      key: '#',
+      title: "#",
+      key: "#",
       render: (_: any, __: UserData, index: number) => {
         return (currentPage - 1) * pageSize + index + 1;
       },
     },
     {
-      title: 'Admin Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: "Admin Name",
+      dataIndex: "name",
+      key: "name",
       render: (text: string) => <a>{text}</a>,
     },
     {
-      title: 'Email ID',
-      dataIndex: 'email',
-      key: 'email',
+      title: "Email ID",
+      dataIndex: "email",
+      key: "email",
     },
     {
-      title: 'Role',
-      dataIndex: 'role',
-      key: 'role',
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
     },
     {
-      title: 'Signup Date',
-      dataIndex: 'signupDate',
-      key: 'signupDate',
-      render: (text: string) => <span>{text || 'N/A'}</span>,
+      title: "Signup Date",
+      dataIndex: "signupDate",
+      key: "signupDate",
+      render: (text: string) => <span>{text || "N/A"}</span>,
     },
     {
-      title: 'Actions',
-      key: 'actions',
+      title: "Actions",
+      key: "actions",
       render: (_: any, record: UserData) => (
         <Space size="middle">
-          <Button type="link" icon={<InfoCircleOutlined />} onClick={() => handleInfoClick(record)} />
+          <Button
+            type="link"
+            icon={<InfoCircleOutlined />}
+            onClick={() => handleInfoClick(record)}
+          />
           {/* <Button type="link" icon={<EditOutlined className="text-black dark:text-white" />} /> */}
-          <Button type="link" icon={<DeleteOutlined className="text-red-500" />} />
+          <Button
+            type="link"
+            icon={<DeleteOutlined className="text-red-500" />}
+          />
         </Space>
       ),
     },
@@ -207,7 +277,7 @@ const UsersTable: React.FC = () => {
             setPageSize(pageSize || 5);
           },
           showSizeChanger: true,
-          pageSizeOptions: ['5', '10', '20'],
+          pageSizeOptions: ["5", "10", "20"],
         }}
       />
       <Modal
@@ -219,50 +289,86 @@ const UsersTable: React.FC = () => {
       >
         {modalData && formData && (
           <Card
-            title={<span style={{ fontWeight: 'bold' }}>{isEditing ? 'Edit User' : formData.name}</span>}
+            title={
+              <span style={{ fontWeight: "bold" }}>
+                {isEditing ? "Edit User" : formData.name}
+              </span>
+            }
             bordered={true}
-            style={{ width: '100%', borderRadius: '8px' }}
-            bodyStyle={{ padding: '24px' }}
+            style={{ width: "100%", borderRadius: "8px" }}
+            bodyStyle={{ padding: "24px" }}
             actions={[
-              <Row justify="space-between" style={{ padding: '0 16px' }} key={"action-row"}>
+              <Row
+                justify="space-between"
+                style={{ padding: "0 16px" }}
+                key={"action-row"}
+              >
                 {isEditing ? (
                   <>
-                    <Button key="cancel" onClick={handleCancelClick} style={{ borderRadius: '8px' }}>Cancel</Button>
-                    <Button key="save" type="primary" onClick={handleSaveClick} style={{ borderRadius: '8px' }}>
+                    <Button
+                      key="cancel"
+                      onClick={handleCancelClick}
+                      style={{ borderRadius: "8px" }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      key="save"
+                      type="primary"
+                      onClick={handleSaveClick}
+                      style={{ borderRadius: "8px" }}
+                    >
                       Save
                     </Button>
                   </>
                 ) : (
                   <>
-                    <Button key="cancel" onClick={handleModalCancel} style={{ borderRadius: '8px' }}>Cancel</Button>
-                    <Button key="edit" type="primary" onClick={handleEditClick} style={{ borderRadius: '8px' }}>
+                    <Button
+                      key="cancel"
+                      onClick={handleModalCancel}
+                      style={{ borderRadius: "8px" }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      key="edit"
+                      type="primary"
+                      onClick={handleEditClick}
+                      style={{ borderRadius: "8px" }}
+                    >
                       Edit User
                     </Button>
                   </>
                 )}
-              </Row>
+              </Row>,
             ]}
           >
             <Form layout="vertical">
-              <Form.Item label={<span style={{ fontWeight: 'bold' }}>User Name</span>}>
+              <Form.Item
+                label={<span style={{ fontWeight: "bold" }}>User Name</span>}
+              >
                 <Input
                   name="name"
                   value={formData.name}
                   readOnly={!isEditing}
                   onChange={handleFormChange}
                   bordered={isEditing}
-                  style={{ padding: isEditing ? '4px 11px' : 0 }}
+                  style={{ padding: isEditing ? "4px 11px" : 0 }}
                 />
               </Form.Item>
-              <Form.Item label={<span style={{ fontWeight: 'bold' }}>Email Address</span>}>
-                <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+              <Form.Item
+                label={
+                  <span style={{ fontWeight: "bold" }}>Email Address</span>
+                }
+              >
+                <div style={{ display: "flex", alignItems: "flex-start" }}>
                   <Input
                     name="email"
                     value={formData.email}
                     readOnly={!isEditing}
                     onChange={handleFormChange}
                     bordered={isEditing}
-                    style={{ padding: isEditing ? '4px 11px' : 0 }}
+                    style={{ padding: isEditing ? "4px 11px" : 0 }}
                   />
                   {!isEditing && (
                     <Button
@@ -274,24 +380,28 @@ const UsersTable: React.FC = () => {
                   )}
                 </div>
               </Form.Item>
-              <Form.Item label={<span style={{ fontWeight: 'bold' }}>Phone Number</span>}>
+              <Form.Item
+                label={<span style={{ fontWeight: "bold" }}>Phone Number</span>}
+              >
                 <Input
                   name="phoneNumber"
                   value={formData.phoneNumber}
                   readOnly={!isEditing}
                   onChange={handleFormChange}
                   bordered={isEditing}
-                  style={{ padding: isEditing ? '4px 11px' : 0 }}
+                  style={{ padding: isEditing ? "4px 11px" : 0 }}
                 />
               </Form.Item>
-              <Form.Item label={<span style={{ fontWeight: 'bold' }}>Role</span>}>
+              <Form.Item
+                label={<span style={{ fontWeight: "bold" }}>Role</span>}
+              >
                 <Input
                   name="role"
                   value={formData.role}
                   readOnly={!isEditing}
                   onChange={handleFormChange}
                   bordered={isEditing}
-                  style={{ padding: isEditing ? '4px 11px' : 0 }}
+                  style={{ padding: isEditing ? "4px 11px" : 0 }}
                 />
               </Form.Item>
             </Form>
