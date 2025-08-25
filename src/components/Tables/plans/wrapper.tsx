6d7c64afc570d42cfb12f5530f4e76f42d79097f
Nirjhor3029 <sazzad3029@gmail.com>
2025-08-25 11:12:55 +0600

    remove destinations from plans table

--- src/components/Tables/plans/wrapper.tsx
+++ src/components/Tables/plans/wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import axiosInstance from "@/api/axios";
-import { getCountriesNameByCodes } from "@/utils/countries";
+import Loading from "@/components/ui/Loading";
 import { DownOutlined } from "@ant-design/icons";
 import {
   Button,
@@ -15,7 +15,6 @@ import {
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import PlansTable from "."; // Import your PlansTable component
-import Loading from "@/components/ui/Loading";
 
 const Wrapper = () => {
   const [selectedCountry, setSelectedCountry] = useState<string>("USA");
@@ -104,9 +103,9 @@ const Wrapper = () => {
           //   .filter(Boolean); // remove null/undefined if code not found
           // console.log(destinations);
 
-          const destinations = getCountriesNameByCodes(
-            plan.supported_countries,
-          );
+          // const destinations = getCountriesNameByCodes(
+          //   plan.supported_countries,
+          // );
           return {
             ...plan,
             dataOrValidity: `${(plan.data_usage_allowance / 1024 ** 3).toFixed(0)} GB, ${formatValidity(plan.time_allowance)}`,
@@ -118,7 +117,7 @@ const Wrapper = () => {
             ).toLocaleString(),
             createdDate: new Date(plan.created_date).toLocaleString(),
             modifiedDate: new Date(plan.modified_date).toLocaleString(),
-            destination: destinations,
+            // destination: destinations,
           };
         });
 
